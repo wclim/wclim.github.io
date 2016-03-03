42c9d6655f3b2ec8983966fd0330cfc4444187c5
wclim <wclim@hotmail.sg>
2016-03-03 22:31:52 +0800

    updated some images

--- assets/js/webSiteState.js
+++ assets/js/webSiteState.js
@@ -3,7 +3,7 @@ if (webSite.state == websiteStates.OFFLINE){
 	jQuery('body').html("<div class='constructionMessage'>Sorry the website is offline. Do check back again!<br><img src='images/offline.png' width='100px'/></div>");
 } else if (webSite.state == websiteStates.UNDERCONSTRUCTION){
 	$('body').addClass("underConstruction");
-	jQuery('body').html("<div class='constructionMessage'>Sorry the website is under construction. Do check back again!<br><img src='images/spannerscrew.png' width='100px'/></div>");
+	jQuery('body').html("<div class='constructionMessage'>Sorry the website is under construction. Do check back again!<br><img src='images/spanner.png' width='100px'/></div>");
 } else if (webSite.state == websiteStates.ACTIVE){
 	
 }
