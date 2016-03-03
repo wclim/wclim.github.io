3fc4e34b737e00cc726ecdf3000a1565ff824771
wclim <wclim@hotmail.sg>
2016-03-03 20:52:10 +0800

    added new stuff
    admin controls for js file, and a website object

---
+++ assets/js/admin.js
@@ -0,0 +1,9 @@
+websiteStates = {
+	OFFLINE: 0,
+	ACTIVE: 1,
+	UNDERCONSTRUCTION: 2
+}
+
+var webSite = {
+	state: 2
+}
--- assets/js/websiteconstruction.js
+++
@@ -1,6 +0,0 @@
-var underConstruction = true;
-
-if (underConstruction){
-	$('body').addClass("underConstruction");
-	jQuery('body').html("<div class='constructionMessage'>Sorry the website is under construction. Do check back again!<br><img src='images/spannerscrew.png' width='100px'/></div>");
-}
