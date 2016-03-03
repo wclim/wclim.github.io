a63945cf72301ad5c3ab38fa883bf4be5478ded7
wclim <wclim@hotmail.sg>
2016-03-03 20:34:47 +0800

    added under construction page
    new construction page for revamp of site

---
+++ assets/js/websiteconstruction.js
@@ -0,0 +1,6 @@
+var underConstruction = true;
+
+if (underConstruction){
+	$('body').addClass("underConstruction");
+	jQuery('body').html("<div class='constructionMessage'>Sorry the website is under construction. Do check back again!<br><img src='images/spannerscrew.png' width='100px'/></div>");
+}
