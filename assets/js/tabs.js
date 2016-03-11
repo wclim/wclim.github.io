67156f49f3f402744571f94d595fd50f5b4b8779
wclim <wclim@hotmail.sg>
2016-03-12 01:10:36 +0800

    add pages for tabs and a lightbox for the pages

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,3 +1,18 @@
 function openLink(link){
-	console.log("Opening: " + link);
+	$('#dark').fadeIn("200", function(){
+		$('#tab').fadeIn("500");
+	});
+	removeEventListener("keydown", keyDownListener, false);
+	removeEventListener("keyup", keyUpListener, false);
+	keysDown = {};
+	document.getElementById('tab').src = "pages/" + link + ".html";
+}
+
+$('#dark').click(closeLink);
+
+function closeLink(){
+	$('#dark').fadeOut("200");
+	$('#tab').fadeOut("200");
+	addEventListener("keydown", keyDownListener, false);
+	addEventListener("keyup", keyUpListener, false);
 }
